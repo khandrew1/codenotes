088c863bf50eddbc2a8d019896c862000d1fd195
Stanley Lew <stanley43595@gmail.com>
2024-04-20 05:42:42 -0700

    added app directory files

---
+++ src/app/instructor/assignments/page.js
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div>
+        hello world! says stanley
+    </div>
+  );
+};
+export default Page;
---
+++ src/app/instructor/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+      <div>
+          hello world! says stanley
+      </div>
+    );
+  };
+  export default Page;
+  
