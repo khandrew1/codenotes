b617c67df0dde1af72c2051ca09cf0a5feeac873
Stanley Lew <stanley43595@gmail.com>
2024-04-20 04:50:56 -0700

    more folders

--- src/components/student/classdashboard/classsidebar.jsx
+++ src/components/student/classdashboard/classsidebar.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import logo from "../../../public/logo.svg";
+import logo from "../../../../public/logo.svg";
 
-const Sidebar = () => {
+const ClassSidebar = () => {
   return (
     <div className="w-2/12 flex flex-col pt-4 px-4 border-r-codenotes-darkgrey border-[1px] h-screen">
       <Image src={logo} alt="logo" />
-      <p className="font-bold py-4 text-lg">Your Courses</p>
-      <p>Welcome to Codenotes! Click on one of courses on the right.</p>
+      <p className="font-bold py-4 text-lg">Class Dashboard</p>
+      <p>
+        Access all your assignments. Click an assignment to turn in or resubmit
+        an assignment.
+      </p>
       <div className="mt-auto">
         <button className="flex items-center justify-center border-[2px] border-codenotes-darkgrey text-codenotes-darkgrey rounded-lg w-full mb-4 py-2 shadow-xl">
           logout
@@ -16,4 +19,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar;
+export default ClassSidebar;
--- src/components/student/dashboard/sidebar.jsx
+++ src/components/student/dashboard/sidebar.jsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
-import logo from "../../../public/logo.svg";
+import logo from "../../../../public/logo.svg";
 
-const ClassSidebar = () => {
+const Sidebar = () => {
   return (
     <div className="w-2/12 flex flex-col pt-4 px-4 border-r-codenotes-darkgrey border-[1px] h-screen">
       <Image src={logo} alt="logo" />
-      <p className="font-bold py-4 text-lg">Class Dashboard</p>
-      <p>
-        Access all your assignments. Click an assignment to turn in or resubmit
-        an assignment.
-      </p>
+      <p className="font-bold py-4 text-lg">Your Courses</p>
+      <p>Welcome to Codenotes! Click on one of courses on the right.</p>
       <div className="mt-auto">
         <button className="flex items-center justify-center border-[2px] border-codenotes-darkgrey text-codenotes-darkgrey rounded-lg w-full mb-4 py-2 shadow-xl">
           logout
@@ -19,4 +16,4 @@ const ClassSidebar = () => {
   );
 };
 
-export default ClassSidebar;
+export default Sidebar;
