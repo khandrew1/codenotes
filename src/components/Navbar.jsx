0621f1e9467bfd463ae32b02be3dde40e32e59fb
Andrew Khadder <andrewkh15@gmail.com>
2024-04-20 14:50:36 -0700

    import sign in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/Logo.png";
+import { signIn } from "next-auth/react";
 
 const Navbar = () => {
   return (
