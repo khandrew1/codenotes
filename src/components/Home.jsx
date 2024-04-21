eda76da95922008dddbdbe8eb72ba0013c1bbd47
fardinzam <fardinzaman51@gmail.com>
2024-04-20 22:23:20 -0700

    new home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,10 @@
+import { GoogleGeminiEffect } from "./ui/Gemini-effect";
+
 const Content = () => {
   return (
     <div className="w-full pt-20">
-      <div>
-        <p className="pt-4 text-codenotes-ooze text-6xl font-bold text-center">
-          Saving the World
-        </p>
-        <p className="text-codenotes-ooze text-3xl font-bold text-center">
-          one assignment at a time
-        </p>
-      </div>
+      {/* <GoogleGeminiEffect /> */}
+      home page
     </div>
   );
 };
