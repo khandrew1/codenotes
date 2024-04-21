56e846583c6be7811419bbe6ba9f1180f41df903
Stanley Lew <stanley43595@gmail.com>
2024-04-21 05:16:54 -0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
-import darkmodelogo from "../../public/darkmodelogo.svg";
+import widelogo from "../../public/widelogo.svg";
 import Image from "next/image";
 const Footer = () => {
   return (
-    <div className="bg-codenotes-black-100 grid grid-cols-2 py-2 text-codenotes-offwhite">
-      <div className="w-4/12">
-        <Image src={darkmodelogo} alt={darkmodelogo} className="w-2/12" />
+    <div className="bg-codenotes-black-100 grid grid-cols-2 py-2 text-codenotes-offwhite px-8">
+      <div className="w-4/12 inline-flex items-center">
+        <Image src={widelogo} alt={widelogo} className="w-8/12" />
       </div>
-
-      <p className="flex items-center !m-0">
-        &copy; 2024 Codenotes, LLC. All rights reserved.
-      </p>
+      <div className="flex justify-end">
+        <p className="flex items-center !m-0">
+          &copy; 2024 Codenotes, LLC. All rights reserved.
+        </p>
+      </div>
     </div>
   );
 };
